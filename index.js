334855d885dc16d4bd0d3ffe5d9ae9968acf821a
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-22 14:18:49 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+function findFunction(record){
+    return (record.result === "W")
+}
 
+function superbowlWin(record){
+    const finder = record.find(findFunction)
+    return (finder === undefined ? undefined : finder.year)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function superbowlWin(arr) {
+//     const finder = arr.find(arr => arr.result === 'W')
+//     return (finder === undefined ? undefined : finder.year)
+// }
